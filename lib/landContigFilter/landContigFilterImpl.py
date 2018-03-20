e577261c2ddea3b7d7cefa96ca90aa6ce5c72654
landml <landml@ornl.gov>
2018-03-20 16:59:58 -0400

    Remove the object created

--- lib/landContigFilter/landContigFilterImpl.py
+++ lib/landContigFilter/landContigFilterImpl.py
@@ -400,7 +400,7 @@ This sample module contains one small method - filter_contigs.
         report_params = {'message': string,
                          'direct_html_link_index': 0,
                          'html_links': [html_file],
-                         'file_links': output_file,
+                         'file_links': [output_file],
                          'report_object_name': 'assembly_metadata_report_' + str(uuid.uuid4()),
                          'workspace_name': params['workspace_name']
                         }        
